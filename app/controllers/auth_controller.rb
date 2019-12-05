eeb2942b4fc44b54ca45e5fcfe06e23d0a1e6464
Saji Ayahito <ayahito_saji@outlook.jp>
2019-12-05 21:54:45 +0900

    ログイン実装

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,3 +1,6 @@
+require 'bcrypt'
+require 'securerandom'
+
 class AuthController < ApplicationController
   def login
     if params[:redirect_to]
@@ -9,21 +12,49 @@ class AuthController < ApplicationController
 
   def callback
     if params[:code]
-      resp = Slack.oauth_access({client_id: ENV["SLACK_CLIENT_ID"], client_secret: ENV["SLACK_CLIENT_SECRET"], code: params[:code], scope: "users:read,users:read.email"})
 
-      user_id = resp["user_id"]
+      # OAuthのリダイレクト先がlocalhost:3000だとリダイレクト拒否されるため，SLACK_USER_ID_FOR_LOCALHOSTを利用してログインする
+      if request.host == "localhost"
+        user_id = ENV["SLACK_USER_ID_FOR_LOCALHOST"]
+      else
+        resp = Slack.oauth_access({client_id: ENV["SLACK_CLIENT_ID"], client_secret: ENV["SLACK_CLIENT_SECRET"], code: params[:code], scope: "users:read,users:read.email"})
+        user_id = resp["user_id"]
+      end
 
       if user_id
         resp = Slack.users_info(:user => user_id)
 
         puts(resp)
 
-        auth_id = "id"
-        auth_token = "token"
+        user = User.find_by(slack_id: resp['user']['id'])
+        if user.nil?
+          user = User.new(
+                  slack_id: resp['user']['id'],
+                  nickname: resp['user']['profile']['display_name'],
+                  email: resp['user']['profile']['email']
+          )
 
-        redirect_to "#{params[:state]}?status=ok&auth_id=#{auth_id}&auth_token=#{auth_token}"
+          if resp['user']['real_name'].split(/\s/)[1]
+            first_name = resp['user']['real_name'].split(/\s/)[1];
+            last_name = resp['user']['real_name'].split(/\s/)[0];
+          else
+            first_name = resp['user']['real_name'].substr(2);
+            last_name = resp['user']['real_name'].substr(0, 2);
+          end
+          user.first_name = first_name
+          user.last_name = last_name
+        end
+
+        user.slack_image = resp['user']['profile']['image_original']
+
+        auth_token = SecureRandom.urlsafe_base64(32)
+
+        user.auth_token = BCrypt::Password.create(auth_token)
+
+        user.save()
+
+        redirect_to "#{params[:state]}?status=ok&auth_id=#{user.id}&auth_token=#{auth_token}"
       else
-        puts resp
         redirect_to "#{params[:state]}?status=error"
       end
     else
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     scope '/v1' do
       get 'auth/login'
       get 'auth/callback'
+
+      resources :users, only: [:index, :show, :update, :destroy]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
